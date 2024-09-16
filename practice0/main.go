a862d243d0fb5dcd1571c6203c78c7254e9b5924
KAlina28 <krylovaalina28@yandex.ru>
2024-09-16 12:46:18 +0300

    fix: go func, task done

--- practice0/main.go
+++ practice0/main.go
@@ -1,7 +1,44 @@
 package main
 
+import (
+	"fmt"
+	// "math/rand"
+	"time"
+)
+
 func main() {
+	b := [100]byte{}
+	go func() {
+		for {
+			filler(b[:50], '0', '1')
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			filler(b[50:], 'X', 'Y')
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			fmt.Println(string(b[:]))
+			time.Sleep(time.Second)
+		}
+	}()
+
+	select {}
 }
 
 func filler(b []byte, ifzero byte, ifnot byte) {
+	for i := 0; i < len(b); i++ {
+		if i%2 == 0 {
+			b[i] = ifzero
+		} else {
+			b[i] = ifnot
+		}
+		//или так для рандома :) b[i] = map[bool]byte{true: ifzero, false: ifnot}[rand.Intn(2) == 0]
+	}
 }
--- practice0/main_test.go
+++ practice0/main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"slices"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFiller(t *testing.T) {
@@ -10,4 +13,6 @@ func TestFiller(t *testing.T) {
 	one := byte('1')
 	filler(b[:], zero, one)
 	// Заполнить здесь ассерт, что b содержит zero и что b содержит one
+	assert.True(t, slices.Contains(b[:], zero), "expected zero in b")
+	assert.True(t, slices.Contains(b[:], one), "expected one in b")
 }
